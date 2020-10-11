97c2dca94191b2fab103d25c2cd3bfd982489c1f
João Rosmaninho <jv_ros@hotmail.com>
2020-10-10 21:47:43 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
 function App() {
+ let numero = 0;
+  function clicou(dividir){
+    numero = numero + 1;
+    let dividido;
+    if(dividir === true){
+     dividido = numero/2;
+    }
+    alert(numero);
+    alert(dividido);
+  }
   return (
-    <div >
- lululul
-    </div>
+    <div>
+ <h1>Hello World</h1>
+ <button onClick={()=>clicou(false)}>+ um</button>
+ <button onClick={()=>clicou(true)}>+ um e divide</button>
+     </div>
   );
 }
 
