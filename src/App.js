1be57b61f63a5c6c13dbeccfc628211bc5b07cbf
João Rosmaninho <jv_ros@hotmail.com>
2020-10-17 19:36:58 -0300

    Criando o routes.js e os componentes das páginas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React from 'react';
+import React from "react";
+import Routes from "./routes"
 function App() {
- let numero = 0;
-  function clicou(dividir){
-    numero = numero + 1;
-    let dividido;
-    if(dividir === true){
-     dividido = numero/2;
-    }
-    alert(numero);
-    alert(dividido);
-  }
-  return (
-    <div>
- <h1>Hello World</h1>
- <button onClick={()=>clicou(false)}>+ um</button>
- <button onClick={()=>clicou(true)}>+ um e divide</button>
-     </div>
-  );
+
+  return <Routes/>;
 }
 
 export default App;
