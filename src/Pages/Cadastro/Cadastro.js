2f770c01024222ef3fa33bd25ffb01044a27d37c
João Rosmaninho <jv_ros@hotmail.com>
2020-10-17 19:55:12 -0300

    Navegação entre rotas

--- src/Pages/Cadastro/Cadastro.js
+++ src/Pages/Cadastro/Cadastro.js
@@ -1,5 +1,9 @@
 import React from "react";
 function Cadastro (){
-return <h1>Cadastro</h1>;
+return (
+<div>
+<h1>Cadastro</h1>
+</div>
+)
 }
 export default Cadastro;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 function Home (){
-return <h1>Home</h1>;
+const history = useHistory();
+return (
+<div>
+    <h1>Home</h1>
+    <button onClick = {() => (history.push("login"))}>Logout</button>
+</div>
+)
 }
 export default Home;
