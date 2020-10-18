ba1f7e80b9ff515903d3366d52362b354e583054
João Rosmaninho <jv_ros@hotmail.com>
2020-10-18 18:04:37 -0300

    Html Css

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Form} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import "./Login.css";
+
 function Login (){
 return (
-<div>
-<h1>Login</h1>
-<Link to="home">
-Home
-</Link>
+<div className = "base">
+<div className ="container">
+    <div>
+<h1 >Quick notes</h1>
+<Form className ="inputs">
+<Form.Group controlId="formBasicEmail">
+    <Form.Control type="email" placeholder="Email" />
+  </Form.Group>
+  <Form.Group controlId="senha">
+    <Form.Control type="password" placeholder="Senha" />
+  </Form.Group>
+  <Button variant="primary">Login</Button>{' '}
+</Form>
+</div>
+</div>
 </div>
 )
 }
