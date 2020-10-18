dc72b70fa81da211c1a5e3a295db54fca5a3f368
João Rosmaninho <jv_ros@hotmail.com>
2020-10-18 18:16:20 -0300

    Login funcional

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import {Form} from "react-bootstrap";
 import {Button} from "react-bootstrap";
+import{useHistory} from "react-router-dom";
 import "./Login.css";
 
 function Login (){
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const history = useHistory();
+    function login (){
+        alert("Bem vindo!" +email)
+        history.push("home");
+    }
 return (
 <div className = "base">
 <div className ="container">
@@ -11,12 +19,12 @@ return (
 <h1 >Quick notes</h1>
 <Form className ="inputs">
 <Form.Group controlId="formBasicEmail">
-    <Form.Control type="email" placeholder="Email" />
+    <Form.Control type="email" placeholder="Email" onChange = {(e) => setEmail(e.target.value)} />
   </Form.Group>
   <Form.Group controlId="senha">
-    <Form.Control type="password" placeholder="Senha" />
+    <Form.Control type="password" placeholder="Senha" onChange = {(e) => setPassword(e.target.value)} />
   </Form.Group>
-  <Button variant="primary">Login</Button>{' '}
+  <Button variant="primary" onClick ={() => login()}>Login</Button>{' '}
 </Form>
 </div>
 </div>
